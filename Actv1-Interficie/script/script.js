c8436a63749e9f5cc9fb60ee391c7f834530798c
AlexVisuals <apoyatosmartin@gmail.com>
2025-12-15 16:59:21 +0100

    responsibe

--- Actv1-Interficie/script/script.js
+++ Actv1-Interficie/script/script.js
@@ -1,6 +1,10 @@
+// Selecciono el botón hamburguesa
 const navToggle = document.querySelector('.nav-toggle');
-const navList = document.querySelector('.nav-list');
 
+// la lista de enlaces del menú
+const navList = document.querySelector('.nav-list');
+// cuando hace  click en el botón hamburguesa
 navToggle.addEventListener('click', () => {
+    // Al hacer click, añadimos o quitamos la clase que muestra/oculta el menú
     navList.classList.toggle('nav-list--open');
 });
